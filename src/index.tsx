519800a35a6bb2d4808192749a417ea0ce33dc2c
Juho Jokela <juho.jokela@atalent.fi>
2018-11-03 13:07:56 +0200

    Add classnames package

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,23 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 
+import { navGroup, navItem, topbar } from "./emotion-styles/src/topbar";
+
 ReactDOM.render(
   <Provider store={store}>
-    <div>{"CS-E4400"}</div>
+    <div>
+      <div className={topbar}>
+        <nav className={navGroup}>
+          <a className={navItem} href="#">Hello world!</a>
+          <a className={navItem} href="#">abc</a>
+          <a className={navItem} href="#">one two three</a>
+        </nav>
+        <nav className={navGroup}>
+          <a className={navItem} href="#">Log in / Sign up</a>
+        </nav>
+      </div>
+      <div>{"CS-E4400"}</div>
+    </div>
   </Provider>,
   document.getElementById("app")
 );
