bb03639a3c492d3cf24defecc34ed2ff6c3c6b9f
Juho Jokela <juho.jokela@atalent.fi>
2018-11-03 13:41:54 +0200

    Add temporary static redux state.

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,47 @@
 import { combineReducers, createStore } from "redux";
 
-export default createStore(combineReducers({}));
+// Temporary static redux state.
+const initialState = {
+  organisation: {
+    appliances: [],
+    id: 1,
+    maintainer: [],
+  },
+  user: {
+    email: "petri.vuorimaa@aalto.fi",
+    first: "Petri",
+    last: "Vuorimaa",
+    organisations: [
+      {
+        id: 1,
+        roles: ["user", "admin"],
+      }
+    ]
+  },
+  views: {
+    appliance: {
+      tabs: {
+        listing: {
+          activeDrawer: "summary",
+          content: "<ApplianceListing drawer={activeDrawer} applianceID={appliance} />",
+          label: "Appliance list",
+        },
+        1: { // Appliance id
+          activeDrawer: "summary",
+          content: "<Appliance id={id} />",
+          label: "My first appliance",
+        }
+      },
+    },
+    maintainer: {
+      tabs: {
+        listing: "ReactComponent",
+      }
+    }
+  },
+};
+
+export default createStore(
+  combineReducers({}),
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
