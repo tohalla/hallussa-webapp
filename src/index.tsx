7c5e6fa3c15a111dd99ce0c1ebb362f48fa17e34
Juho Jokela <juho.jokela@atalent.fi>
2018-11-04 10:44:58 +0200

    Change index to display ApplianceView (temporarily)

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,12 @@ import { Provider } from "react-redux";
 import store from "./store";
 
 import { navGroup, navItem, topbar } from "./emotion-styles/src/topbar";
+import ApplianceView from "./views/ApplianceView";
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
-      <div className={topbar}>
-        <nav className={navGroup}>
-          <a className={navItem} href="#">Hello world!</a>
-          <a className={navItem} href="#">abc</a>
-          <a className={navItem} href="#">one two three</a>
-        </nav>
-        <nav className={navGroup}>
-          <a className={navItem} href="#">Log in / Sign up</a>
-        </nav>
-      </div>
+      <ApplianceView />
       <div>{"CS-E4400"}</div>
     </div>
   </Provider>,
