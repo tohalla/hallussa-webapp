01b115b4afa860334a8d6607dda0b69a122630e7
Hai Phan <minh.phan@aalto.fi>
2018-11-08 16:57:36 +0200

    created appliances views

--- src/appliances.tsx
+++ src/appliances.tsx
@@ -1,10 +1,38 @@
+import { css } from "emotion";
+import container, { innerContent, listContainer, summaryContainer } from "emotion-styles/container";
+import { lightText } from "emotion-styles/inline";
+import { actionTab, activeTab, unactiveTab } from "emotion-styles/tabbed";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { colors, text } from "./emotion-styles/src/variables/colors";
 
 const Appliances = () =>
-  <div>
-    <h2>Appliances</h2>
+  <div >
+    <div>
+      <h3 className={activeTab}>Appliances</h3>
+      <h3 className={unactiveTab}>Maximus pontimus</h3>
+      <h3 className={actionTab}>+</h3>
+    </div>
+
+    <div>
+      <div className={listContainer}>
+        <h2>list</h2>
+      </div>
+      <div className={summaryContainer}>
+        <div>
+          <div>
+            <h2>Summary</h2>
+          </div>
+          <div>
+            <h2>Latest Activity</h2>
+          </div>
+          <div>
+            <h2>Statistics</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>;
 
 export default Appliances;
