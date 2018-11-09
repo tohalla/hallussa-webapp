274edb44d3692afa6078c826debade09a6dfc49c
Juho Jokela <juho.jokela@atalent.fi>
2018-11-09 14:22:53 +0200

    Add root router for testing tabs

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import store from "./store";
 
-import ApplianceView from "./appliances/view/ApplianceView";
-import { navGroup, navItem, topbar } from "./emotion-styles/src/topbar";
+import ApplianceView from "./appliances/router";
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
-      <ApplianceView />
-      <div>{"CS-E4400"}</div>
+      <Router>
+        <div>
+          <Route path="/appliances" component={ApplianceView} />
+        </div>
+      </Router>
     </div>
   </Provider>,
   document.getElementById("app")
