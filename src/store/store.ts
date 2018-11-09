033ae32540d7327fb93cbc4b3114becf18a9078f
Juho Jokela <juho.jokela@atalent.fi>
2018-11-09 16:26:20 +0200

    Add initialState to store and appliance reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,17 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 
+import initialState from "./initialState";
 import api from "./middleware/api";
 
+import appliances from "../appliances/tabs/reducer";
+
 export default createStore(
-  combineReducers({}),
+  combineReducers({
+    views: combineReducers({
+      appliances,
+    }),
+  }),
+  initialState,
   applyMiddleware(thunk, api)
 );
