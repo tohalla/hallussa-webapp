679069f5b20eb84514b799784382edf8e992dece
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-10 17:55:19 +0200

    add styles to navigation

--- src/navigation/TopBar.tsx
+++ src/navigation/TopBar.tsx
@@ -1,14 +1,23 @@
-import { topbar } from "emotion-styles/topbar";
-import React from "react";
+import { navGroup, navItem, topbar } from "emotion-styles/topbar";
+import React, { MouseEventHandler } from "react";
 import { Link } from "react-router-dom";
 
+import { signOut } from "../auth/auth";
+import Button from "../components/Button";
+
 export default class Topbar extends React.Component {
+  public handleLogout = () => signOut();
+
   public render() {
     return (
       <div className={topbar}>
-        <Link to="/appliances">Appliances</Link>
-        <Link to="/maintainers">Maintainers</Link>
-        <a href="#">Log out</a>
+        <div className={navGroup}>
+          <Link className={navItem} to="/appliances">Appliances</Link>
+          <Link className={navItem} to="/maintainers">Maintainers</Link>
+        </div>
+        <div className={navGroup}>
+          <Button plain={true} onClick={this.handleLogout}>Log out</Button>
+        </div>
       </div>
     );
   }
