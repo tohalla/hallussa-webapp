1db21f4aa62f1164da532561143bed7a510a8b43
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-11 21:24:21 +0200

    add greetings

--- src/navigation/AccountMenu.tsx
+++ src/navigation/AccountMenu.tsx
@@ -2,18 +2,47 @@ import {
   navGroup,
   navItem,
 } from "emotion-styles/topbar";
+import { path } from "ramda";
 import React from "react";
+import { connect } from "react-redux";
 
+import { fetchAccount } from "../account/actions";
 import { signOut } from "../auth/auth";
+import { ReduxAPICall } from "../store/middleware/api";
+import loadable from "../util/hoc/loadable";
 
-export default class AccountMenu extends React.Component {
+interface AccountMenuProps {
+  account: {
+    firstName: string
+  };
+  fetchAccount(): ReduxAPICall;
+}
+
+const mapStateToProps = (state: object) => ({
+  account: path(["account"], state),
+});
+
+export class AccountMenu extends React.Component<AccountMenuProps> {
+  public componentWillMount = () => {
+    this.props.fetchAccount();
+  }
+
   public handleLogout = () => signOut();
 
   public render() {
+    const {firstName} = this.props.account;
     return (
       <div className={navGroup}>
+        Hello, {firstName}
         <a className={navItem} onClick={this.handleLogout}>Log out</a>
       </div>
     );
   }
 }
+
+export default
+  connect<{}, {}, AccountMenuProps>(
+  mapStateToProps, {
+    fetchAccount,
+  }
+)(loadable(AccountMenu));
