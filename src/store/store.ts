6f7826c2a8c7dcc744fc1d2e7f6c6f6ee7cb7657
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-11 21:20:54 +0200

    add account reducer and actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 
+import account from "../account/reducer";
 import api from "./middleware/api";
 
 export default createStore(
-  combineReducers({}),
+  combineReducers({
+    account,
+  }),
   applyMiddleware(thunk, api)
 );
