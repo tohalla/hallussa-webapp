1322ee2eceb95cff496034b3989783805d50dc4b
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-13 14:22:59 +0200

    add action type for setting active account

--- src/account/actions.ts
+++ src/account/actions.ts
@@ -1,4 +1,4 @@
-import { AnyAction } from "redux";
+import { AnyAction, Dispatch } from "redux";
 import { CALL_API, ReduxAPICall } from "../store/middleware/api";
 
 export interface AccountPayload {
@@ -7,6 +7,7 @@ export interface AccountPayload {
   email: string;
 }
 
+export const SET_ACTIVE_ACCOUNT = Symbol("SET_ACTIVE_ACCOUNT");
 export const FETCH_ACCOUNT_REQUEST = Symbol("FETCH_ACCOUNT_REQUEST");
 export const FETCH_ACCOUNT_SUCCESS = Symbol("FETCH_ACCOUNT_SUCCESS");
 export const FETCH_ACCOUNT_FAILURE = Symbol("FETCH_ACCOUNT_FAILURE");
@@ -16,9 +17,16 @@ export interface AccountAction extends AnyAction {
   payload?: Partial<AccountPayload>;
 }
 
-export const fetchAccount = (): ReduxAPICall => ({
+export const setActiveAccount = (payload: any) => ({
+  payload,
+  type: SET_ACTIVE_ACCOUNT,
+});
+
+export const fetchAccount = () => (dispatch: Dispatch) => dispatch<ReduxAPICall>({
   endpoint: "/accounts",
   method: "GET",
+  onSuccess: (payload) => dispatch(setActiveAccount(payload.id)),
+  transformResponse: (response) => ({[response.id]: response}),
   type: CALL_API,
   types: [FETCH_ACCOUNT_REQUEST, FETCH_ACCOUNT_SUCCESS, FETCH_ACCOUNT_FAILURE],
 });
