11858b3d087e5aa57ae6a0abaf5c341c8538bc90
Juho Jokela <juho.jokela@atalent.fi>
2018-11-15 11:23:24 +0200

    Create hoc for views with drawers

---
+++ src/components/hocs/WithSidebarHOC.tsx
@@ -0,0 +1,28 @@
+import React, { Component, ComponentType } from "react";
+
+export default <P extends object>(WrappedComponent: ComponentType<P>) => (
+  class extends Component<P & {[key: string]: any}> {
+    constructor(props: any) {
+      super(props);
+      this.state = {
+        openDrawer: "",
+      };
+    }
+
+    public handleToggle = (view: string, drawer = "") => {
+      // NOTE: If we were to use redux for drawers,
+      // we could move this logic to DrawerLabel or Drawer.
+      this.setState({ openDrawer: drawer });
+    }
+
+    public render() {
+      return (
+        <WrappedComponent
+          toggleActiveDrawer={this.handleToggle}
+          openDrawer={this.state.openDrawer}
+          {...this.props}
+        />
+      );
+    }
+  }
+);
