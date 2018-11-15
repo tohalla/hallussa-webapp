019a4c51dbb0b3ad6fbc080ef63cb58f9e945aaf
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-15 14:43:48 +0200

    add router to auth
    inject global styles

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -2,7 +2,10 @@ import "@babel/polyfill";
 import defaultContainer, { appContainer, centerContent } from "emotion-styles/container";
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import "whatwg-fetch"; // fetch polyfill, replaces standard fetch
+
+import "emotion-styles";
 
 import { baseUrl } from "../config";
 import { authenticate } from "./auth";
@@ -21,7 +24,13 @@ import AuthenticationForm from "./AuthenticationForm";
   ReactDOM.render(
     <div className={centerContent}>
       <div className={defaultContainer}>
-        <AuthenticationForm />
+        <Router>
+          <Switch>
+            <Route exact={true} path={"/"} component={AuthenticationForm} />
+            <Route exact={true} path={"/register"} component={() => "register"} />
+            <Redirect path="*" to="/" />
+          </Switch>
+        </Router>
       </div>
     </div>,
     document.getElementById("app")
