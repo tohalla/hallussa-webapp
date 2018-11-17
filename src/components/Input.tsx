749e1942b47767f842418733269067cdfca5237b
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-17 09:35:36 +0200

    update input element
    allow passing error
    
    add logic checking whether the input is visited or not

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,23 @@
-import input from "emotion-styles/input";
-import React, { ChangeEventHandler, Component } from "react";
+import classnames from "classnames";
+import React, {
+  ChangeEventHandler,
+  Component,
+  FocusEventHandler,
+  RefObject
+} from "react";
+
+import { error as errorStyle } from "emotion-styles/inline";
+import input, { invalid } from "emotion-styles/input";
+import { stackedLayout } from "emotion-styles/layout";
 
 export interface InputProps {
   autoComplete: "off" | "on";
+  error?: string;
   disabled?: boolean;
   name?: string;
+  onBlur?: FocusEventHandler<HTMLInputElement>;
   onChange: ChangeEventHandler<HTMLInputElement>;
+  onFocus?: FocusEventHandler<HTMLInputElement>;
   placeholder?: string;
   type: "text" | "password" | "number" | "date";
   value: string;
@@ -17,7 +29,41 @@ export default class Input extends Component<InputProps> {
     type: "text",
   };
 
+  public element: RefObject<HTMLInputElement>;
+  public visited: boolean = false;
+
+  constructor(props: InputProps) {
+    super(props);
+    this.element = React.createRef();
+  }
+
+  public handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+    const {onBlur} = this.props;
+    this.visited = true;
+    if (typeof onBlur === "function") { onBlur(event); }
+  }
+
+  public handleFocus: FocusEventHandler<HTMLInputElement> = (event) => {
+    const {onFocus} = this.props;
+    if (typeof onFocus === "function") { onFocus(event); }
+    this.forceUpdate(); // re-render to remove error
+  }
+
   public render() {
-    return <input className={input} {...this.props} />;
+    const {error, onBlur, onFocus, ...props} = this.props;
+    const displayError = Boolean(error) && this.visited && document.activeElement !== this.element.current;
+    const className = classnames(input, {[invalid]: displayError});
+    return (
+      <div className={stackedLayout}>
+        <input
+          className={className}
+          ref={this.element}
+          onBlur={this.handleBlur}
+          onFocus={this.handleFocus}
+          {...props}
+        />
+        {displayError && <span className={errorStyle}>{error}</span>}
+      </div>
+    );
   }
 }
