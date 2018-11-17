5ebd0a590e3ef74c944d4f334b387fe7af4a39d7
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-17 09:36:18 +0200

    add validation to password element

--- src/auth/RegistrationForm.tsx
+++ src/auth/RegistrationForm.tsx
@@ -1,11 +1,11 @@
-import React, { ChangeEvent, Component, FormEvent } from "react";
+import { assoc, assocPath } from "ramda";
+import React, { ChangeEvent, Component, FormEvent, RefObject } from "react";
 import { Link } from "react-router-dom";
 
 import Form from "../components/Form";
 import Input from "../components/Input";
-import { baseUrl } from "../config";
 import { small } from "../emotion-styles/src/inline";
-import { authenticate, register } from "./auth";
+import { register } from "./auth";
 
 type Inputs = "email"
   | "firstName"
@@ -13,25 +13,55 @@ type Inputs = "email"
   | "password"
   | "retypePassword";
 
-class RegistrationForm extends Component<{}, { [input in Inputs]?: string }> {
-  public state = {
+type State = {
+  errors: {
+    retypePassword?: string
+  };
+} & {[input in Inputs]: string};
+
+class RegistrationForm extends Component<{}, State > {
+  public state: State = {
     email: "",
+    errors: {},
     firstName: "",
     lastName: "",
     password: "",
     retypePassword: "",
   };
 
+  public retypePasswordRef: RefObject<Input>;
+
+  constructor(props: {}) {
+    super(props);
+    this.retypePasswordRef = React.createRef();
+  }
+
   public handleSubmit = async (event: FormEvent) => register(this.state);
 
   public handleInputChange = (input: Inputs) => (
     event: ChangeEvent<HTMLInputElement>
   ) => {
-    this.setState({ [input]: event.target.value });
+    this.setState(assoc(input, event.target.value));
+  }
+
+  public validate = () => {
+    const retypePasswordInput = this.retypePasswordRef.current as Input;
+    const {retypePassword, password} = this.state;
+
+    if (retypePasswordInput.visited && retypePassword !== password) {
+      this.setState(assocPath(["errors", "retypePassword"], "Passwords do not match"));
+    }
   }
 
   public render() {
-    const { email, password, firstName, lastName, retypePassword } = this.state;
+    const {
+      errors,
+      email,
+      password,
+      firstName,
+      lastName,
+      retypePassword,
+    } = this.state;
     return (
       <Form
         onSubmit={this.handleSubmit}
@@ -66,10 +96,13 @@ class RegistrationForm extends Component<{}, { [input in Inputs]?: string }> {
           value={password}
         />
         <Input
+          error={errors.retypePassword}
           name="re-enter-password"
+          onBlur={this.validate}
           onChange={this.handleInputChange("retypePassword")}
           type="password"
           placeholder="Re-enter password"
+          ref={this.retypePasswordRef}
           value={retypePassword}
         />
       </Form>
