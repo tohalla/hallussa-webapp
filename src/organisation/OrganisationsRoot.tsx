18ccf36ba53d493af0a6d8a74b15c836c1e438bb
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-18 22:40:08 +0200

    remove redundant contentlayout
    add padded styles

--- src/organisation/OrganisationsRoot.tsx
+++ src/organisation/OrganisationsRoot.tsx
@@ -1,12 +1,14 @@
+import classNames from "classnames";
 import React from "react";
 import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+
 import ViewContainer from "../components/layouts/ViewContainer";
-import { viewContentContainer } from "../emotion-styles/src/container";
+import { padded, viewContentContainer } from "../emotion-styles/src/container";
 import Organisation from "./views/Organisation";
 
 export default () => (
   <Router basename="/organisations">
-    <ViewContainer className={viewContentContainer}>
+    <ViewContainer className={classNames(viewContentContainer, padded)}>
       <Switch>
         <Route exact={true} path="/" component={Organisation} />
         <Redirect path="*" to="/" />
