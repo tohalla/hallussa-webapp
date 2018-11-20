dd002153fc7717aefdbd70b578476f7e521a1ca6
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-20 14:09:10 +0200

    connect maintainer creation to reducers

--- src/maintainer/reducer.ts
+++ src/maintainer/reducer.ts
@@ -1,17 +1,19 @@
-import { cond, equals, merge, T } from "ramda";
+import { assoc, cond, equals, merge, T } from "ramda";
 import { Reducer } from "redux";
 
-import { FETCH_MAINTAINERS_SUCCESS, MaintainerAction } from "./actions";
+import { CREATE_MAINTAINER_SUCCESS, FETCH_MAINTAINERS_SUCCESS, MaintainerAction } from "./actions";
 
 const typeHandler = cond([
   [equals(FETCH_MAINTAINERS_SUCCESS), (type, state, payload) => merge(state, payload)],
+  [equals(CREATE_MAINTAINER_SUCCESS), (type, state, payload) =>
+    payload.id ? assoc(String(payload.id), payload, state) : state,
+  ],
   [T, (type, state, payload) => state],
 ]);
 
 const reducer: Reducer<{[key: number]: any}, MaintainerAction> = (
   state = {},
   {payload, type}: MaintainerAction
-) =>
-  typeHandler(type, state, payload);
+) => typeHandler(type, state, payload);
 
 export default reducer;
