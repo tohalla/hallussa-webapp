9513206abac24003420d4548a30f5d45add1d386
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-21 13:38:17 +0200

    update table styles

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactTable, { TableProps } from "react-table";
 import "react-table/react-table.css";
+import { table } from "../emotion-styles/src/table";
 import { greyscale } from "../emotion-styles/src/variables/colors";
 
 type Props = Pick<TableProps, "pageSize" | "columns" | "data">;
 
+const getHeaderProps = () => ({style: {
+  background: greyscale[7],
+  boxShadow: "none",
+  fontWeight: 900,
+}});
+
 export default ({pageSize = 20, ...props}: Props) => (
   <ReactTable
+    className={table}
+    getTheadProps={getHeaderProps}
     minRows={0}
     multiSort={false}
     resizable={false}
