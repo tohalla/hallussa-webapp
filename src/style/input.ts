e943a9d28af55546cb8b74fe4d5d4f3fda66c1cb
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-04-24 22:25:22 +0300

    udpate sets box-shadow to white on form inputs (chrome autocompletion)

--- src/style/input.ts
+++ src/style/input.ts
@@ -1,7 +1,7 @@
 import { css } from "emotion";
 import { stacked } from "./container";
 import { error } from "./inline";
-import { indicator, text } from "./variables/colors";
+import { greyscale, indicator, text } from "./variables/colors";
 import { inputHeight } from "./variables/sizes";
 import { minimal, minor } from "./variables/spacing";
 
@@ -9,6 +9,10 @@ const input = css`
   border: none;
   outline: none;
   padding: ${minimal} ${minor};
+
+  &:-webkit-autofill {
+    box-shadow: 0 0 0 30px ${greyscale[9]} inset !important;
+  }
 
   &:not(textarea) {
     height: ${inputHeight};
