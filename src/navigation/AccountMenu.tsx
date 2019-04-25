beb979cc7c9b9d7b2e9590f3e8295e38afc6310c
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-04-25 23:01:50 +0300

    change greeting to profile (topbar) and update logout to use navitem style

--- src/navigation/AccountMenu.tsx
+++ src/navigation/AccountMenu.tsx
@@ -2,40 +2,30 @@ import classNames from "classnames";
 import { path } from "ramda";
 import React from "react";
 
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { connect, MapStateToProps } from "react-redux";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AccountPayload } from "../account/actions";
 import { signOut } from "../auth/auth";
 import Button from "../component/button/Button";
 import { ReduxState } from "../store/store";
 import { light } from "../style/inline";
-import { navGroup } from "../style/topbar";
+import { activeItem, navGroup, navItem } from "../style/topbar";
 
 interface StateProps {
   account?: AccountPayload;
 }
 
 const AccountMenu = (props: StateProps) => {
-  if (typeof props.account === "undefined") {
-    return null;
-  }
-
   const {t} = useTranslation();
   return (
-    <div className={classNames(navGroup, "horizontal")}>
-      <div className={navGroup}>
-        <span>
-          <Trans i18nKey="navigation.user.greeting" values={{name: props.account.firstName}}>
-            a <Link className={light} to="/account">b</Link>
-          </Trans>
-        </span>
-      </div>
-      <div className={navGroup}>
-        <Button className={light} onClick={signOut} plain={true}>
-          {t("navigation.user.signOut")}
-        </Button>
-      </div>
+    <div className={classNames(navGroup)}>
+      <NavLink activeClassName={activeItem} className={navItem} to="/profile">
+        {t("navigation.item.profile")}
+      </NavLink>
+      <Button className={navItem} onClick={signOut} plain={true}>
+        {t("navigation.user.signOut")}
+      </Button>
     </div>
   );
 };
