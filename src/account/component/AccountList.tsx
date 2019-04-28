0daf654bb153447746909481b19a826270d4b217
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-04-28 21:41:04 +0300

    add user role to account listing

--- src/account/component/AccountList.tsx
+++ src/account/component/AccountList.tsx
@@ -1,28 +1,44 @@
+import { filter, path, prop } from "ramda";
 import React, { ReactFragment } from "react";
 import { Column } from "react-table";
 
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { connect, MapStateToProps } from "react-redux";
 import Table from "../../component/Table";
+import { OrganisationPayload } from "../../organisation/actions";
+import { getOrganisation } from "../../organisation/state";
+import { EntityGroup } from "../../store/reducer";
+import { ReduxState } from "../../store/store";
+import Loadable from "../../util/hoc/Loadable";
 import { AccountPayload } from "../actions";
+import { UserRolePayload } from "../user-role/actions";
+
+interface StateProps {
+  userRoles: EntityGroup<UserRolePayload>;
+}
 
 interface Props {
   accounts: Readonly<AccountPayload[]>;
+  organisation?: OrganisationPayload;
   header?: ReactFragment;
 }
 
-export default ({accounts, header}: Props) => {
+const AccountList = ({accounts, header, userRoles}: Props & StateProps) => {
   const {t} = useTranslation();
-
   const columns: Column[] = [
     {Header: t("account.field.id"), accessor: "id", id: "id", maxWidth: 50},
     {
       Header: t("account.field.name"),
-      accessor: ({id, firstName, lastName}) => <Link to={`/accounts/${id}`}>{firstName} {lastName}</Link>,
+      accessor: ({firstName, lastName}) => `${firstName} ${lastName}`,
       id: "name",
       resizable: true,
     },
     {Header: t("account.field.email"), accessor: "email", id: "email", resizable: true},
+    {
+      Header: t("account.field.role"),
+      accessor: ({userRole}) => t(`role.${path([userRole, "name"], userRoles)}`),
+      id: "userRole",
+    },
   ];
 
   return (
@@ -35,3 +51,20 @@ export default ({accounts, header}: Props) => {
     </>
   );
 };
+
+const mapStateToProps: MapStateToProps<StateProps, Props, ReduxState> = (
+  state,
+  {organisation = getOrganisation(state)}
+) => {
+  const organisationId = prop<any, any>("id", organisation);
+  return {
+    userRoles: filter(
+      (userRole) => userRole.isShared || userRole.organisation === organisationId,
+      state.entities.userRoles
+    ),
+  };
+};
+
+export default connect(
+  mapStateToProps
+)(Loadable<StateProps, Props>(AccountList));
--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -1,7 +1,7 @@
 import "@babel/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import defaultContainer, { appContainer, centerContent } from "style/container";
 import "whatwg-fetch"; // fetch polyfill, replaces standard fetch
 
