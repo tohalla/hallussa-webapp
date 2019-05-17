18a971bb1d2881994afafe98bec36eac5088de6a
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-05-17 20:01:39 +0300

    update removes unused utility function

--- src/util/utilityFunctions.ts
+++ src/util/utilityFunctions.ts
@@ -1,10 +1,6 @@
 import { assocPath } from "ramda";
 import { getAndCheckJWT } from "../auth/auth";
 
-// helper for ramda cond and reducers
-export const anyEquals = <T = any>(items: T[]) => (compare: T) =>
-  items.indexOf(compare) !== -1;
-
 // wrapper to inject authentication headers to fetch request
 export const authenticatedFetch = async (input: RequestInfo, init?: RequestInit) => {
   const token = await getAndCheckJWT();
