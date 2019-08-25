9830b381b21fa9cf18b2851fcfabba9c6059e835
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-08-25 12:11:14 +0300

    update daypicker style

--- src/component/input/DateInput.tsx
+++ src/component/input/DateInput.tsx
@@ -1,8 +1,12 @@
 import { format as dateFNSFormat } from "date-fns";
 import { enGB, enUS, fi } from "date-fns/locale";
+import { css } from "emotion";
 import React from "react";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import { DayPickerInputProps } from "react-day-picker/types/props";
+
+import { greyscale } from "../../style/variables/colors";
+import { minor } from "../../style/variables/spacing";
 
 const locales = {enUS, fi, enGB};
 
@@ -14,12 +18,23 @@ interface Props {
 const formatDate = (date: Date, f: string, locale: keyof typeof locales) =>
   dateFNSFormat(date, f, {locale: locales[locale]});
 
-const format = "MM.DD.YYYY";
+const format = "dd.MM.yyyy";
 
 const DateInput = (props: Props) => (
   <DayPickerInput
+    classNames={{
+      container: "",
+      overlay: "",
+      overlayWrapper: css`
+        position: fixed;
+        margin: ${minor} 0 0 ${minor};
+        background: ${greyscale[9]};
+        border: 1px solid ${greyscale[5]};
+      `,
+    }}
     formatDate={formatDate}
     format={format}
+    inputProps={{placeholder: ""}}
     {...props}
   />
 );
